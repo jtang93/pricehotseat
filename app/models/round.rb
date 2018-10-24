f97ddf806a3290a92ffb7df028eff6f452335b81
mx@E <mglxp5@gmail.com>
2018-10-24 16:20:36 -0400

    Round Has One Item

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,6 +2,6 @@ class Round < ApplicationRecord
   belongs_to :game
   has_many :choices
   has_many :players, :through => :choices
-  has_many :round_items
-  has_many :items, :through => :round_items
+  has_one :round_item
+  has_one :item, :through => :round_item
 end
